0a4f69c88aaf78329f64a42e80181af216132056
Arckos347 <104519680+Arckos347@users.noreply.github.com>
2022-05-20 13:53:36 -0500

    Sesión 21 Practica de Arrays Anidados parte 3

--- Sesión 19/code.js
+++ Sesión 19/code.js
@@ -31,15 +31,23 @@ mostrar.addEventListener('click',mostrarPokemones);
     Definir la función que va a agregar pokemones al array
 */
 
-function agregarPokemon(){
+function agregarPokemon(){ 
     /* Aqui va nuestro codigo */
+    /* Condicional if */
+
+if(nombre.value === '') {
+    /*Codigo que se ejecuta*/
+    mensaje.innerHTML = 'Hey! primero completa los datos y luego agrega un pokemon!!';
+} else {
+    /* Ejecuta esto si no se cumple una condición*/
     //Así agregamos datos a un array con .push
     pokemones.push([ nombre.value , tipo.value , poder.value ]);
  var i = pokemones.length - 1;   
  //Imprimimos el mensaje en la etiqueta p id mensaje
  mensaje.innerHTML = 'Se Agrego ' + pokemones[i][0];
-}
+};
 
+}
 
 /*
     Definir la función que va a mostrar todos los datos del array
@@ -47,12 +55,25 @@ function agregarPokemon(){
 
 function mostrarPokemones(){
     //Código a ejecutar
+    // Condición if/Else
+    if(pokemones.length == 0) {
+        mensaje.innerHTML = 'Para ver tus pokemones, primero agrega uno';  
+    } else {
+/* Ciclo for -> Repetir código*/
+for(var r = 0; r < 3; r++) {
+    
+    /* Codigo que se repite */ 
     document.write(
         '<ul>' +
-        '<li><img src="imagenes/' + pokemones[0][0] + '.png" alt="Imagen Pokemon"></li>' +
-        '<li>Pokemon: ' + pokemones[0][0] + '</li>' +
-        '<li>Tipo: ' + pokemones[0][1] + '</li>' +
-        '<li>Poder: ' + pokemones[0][2] + '</li>' +
-    '</ul>'
-        );
-}
+        '<li><img src="imagenes/' + pokemones[r][0] + '.png" alt="Imagen Pokemon"></li>' +
+        '<li>Pokemon: ' + pokemones[r][0] + '</li>' +
+        '<li>Tipo: ' + pokemones[r][1] + '</li>' +
+        '<li>Poder: ' + pokemones[r][2] + '</li>' +
+        '</ul>'
+    );
+    };
+
+}          
+};
+
+
