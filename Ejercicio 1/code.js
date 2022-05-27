e8073afd2c63a29d1070cad285328723fbcf3f13
Arckos347 <104519680+Arckos347@users.noreply.github.com>
2022-05-27 12:21:53 -0500

    Ejercicio 1

--- Ejercicio 1/code.js
+++ Ejercicio 1/code.js
@@ -1,21 +1,3 @@
-var nombre;
-var edad;
-var futuro;
-var boton;
-
-nombre = document.getElementById("tuNombre");
-edad = document.getElementById("tuEdad");
-futuro = document.getElementById("tuFuturo");
-boton = document.getElementById("miBoton");
-
-boton.addEventListener("click",calcularEdad);
-
-function calcularEdad(){
-    var nacimiento = 2050 + edad.value;
-    var edadFuturo = futuro.value - nacimiento;
-    
- console.log("Hola " + nombre.value + " naciste en el año " + nacimiento + " tu edad en el año " + futuro.value + " sería de " + edadFuturo + " años");
-
-}
-
-console.log
+var name = prompt('¡Cual es tu nombre?')
+var age = parseInt(prompt('¿Cual es tu edad?'));
+var ageFuture = age + 28; 
