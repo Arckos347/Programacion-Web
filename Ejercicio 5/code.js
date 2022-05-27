18fd0b8b45035fe8ea9793255dc71d69617a73ee
Arckos347 <104519680+Arckos347@users.noreply.github.com>
2022-05-27 13:04:52 -0500

    Ejercicio 5 Examen 2do

---
+++ Ejercicio 5/code.js
@@ -0,0 +1,11 @@
+var refresco = prompt('¿Cual te gusta, la Coca cola o la Pepsi?');
+
+if(refresco === 'Coca' ){
+    console.log('Le sabes');
+} else if (refresco === 'Pepsi'){
+    console.log('No le sabes man');
+} else {
+    console.log('Elige la coca o una pepsi');
+}
+
+
