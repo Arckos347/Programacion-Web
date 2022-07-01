816f72750294b44dd8f4fa9d0e33dfe014d6d711
Arckos347 <104519680+Arckos347@users.noreply.github.com>
2022-07-01 13:58:41 -0500

    Sesión 31 clase 01 de julio

---
+++ Sesión 31 clase 01 de julio/script.js
@@ -0,0 +1,37 @@
+/*let games = ['Mario Bross', 'Zelda', 'Donkey Kong', 'Street Fighter', 'Halo'];
+
+console.log(games.sort());
+
+let scores = [1000, 800, 234, 10, 4000, 1];
+
+console.log( scores.sort( (a,b) => a - b ) );
+*/
+
+//let pokemones = ['Pikachu', 'Charmander', 'Boulbasour', 'Dito', 'Snorlax'];
+
+//pokemones.forEach( (pokemon) => console.log(pokemon) )
+
+//pokemones.forEach( (pokemon,index) => console.log(`${pokemon} esta en la posición ${index}`) );
+
+
+//let words = ['HTML', 'CSS', 'PHP', 'JavaScript', 'Git'];
+
+//console.log( words.some(  (word) => word.length == 10 ) );
+//console.log( words.every(  (word) => word.length > 2 ) );
+
+//let number = [1, 2, 3, 4, 5];
+
+//let numbersDoble = numbers.map( (number) => number * 2 );
+
+//console.log(numbersDoble);
+
+//let numbers = [10, 20, 30, 40, 50];
+
+//let numberFilter = numbers.filter( (number) => number <= 30 );
+
+//console.log(numbersFilter)
+
+//let numbers = [1, 2, 3, 4, 5];
+
+//console.log( numbers.reduce( (a,b) => a + b ) );
+//console.log( numbers.reduce( (a,b) => a - b ) );
