c18eff3fe410d94b0140bf780868795b269212f2
Arckos347 <104519680+Arckos347@users.noreply.github.com>
2022-07-07 10:29:37 -0500

    sesion 31

---
+++ Sesión 31 clase 06 de julio/script.js
@@ -0,0 +1,27 @@
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(...numbers);
+
+// const addNumbers = (a,b,c) => {
+//     console.log(a + b + c);
+// }
+
+// let numbers = [1,2,3];
+
+// addNumbers(...numbers);
+
+
+// let pokemones = ['Pikachu', 'Charmander', 'Bulbasaur']
+
+// let newPokemones = ['Ditto', 'Snorlax'];
+
+// pokemones.push(newPokemones);
+
+// console.log(pokemones);
+
+// let numbers1 = [1,2,3,4,5]
+// let copyNumbers = numbers;
+
+// console.log(copyNumbers)
+
