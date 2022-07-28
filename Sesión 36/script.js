e541192f6b429f212d08abeb815ac77f75510172
Arckos347 <104519680+Arckos347@users.noreply.github.com>
2022-07-28 10:04:23 -0500

    Sesión 36 Acceso al DOM
    Alan Pérez Trujillo

---
+++ Sesión 36/script.js
@@ -0,0 +1,14 @@
+//const titulo = document.getElementById('title');
+//console.log(titulo.textContent);
+
+
+
+//titulo.textContent = 'Cambio de Información';
+//console.log(titulo.textContent);
+
+const parrafos = document.querySelectorAll('.paragraph');
+
+
+console.log(parrafos[0].textContent);
+console.log(parrafos[1].textContent);
+console.log(parrafos[2].textContent);
